0806ba7a98b2ba59b03ea9c67f839d8a4ecbb0be
ngaosenpai <longbestlee1909@gmail.com>
2020-06-16 23:49:48 +0700

    edit smt

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,45 +17,48 @@ function Login(){
 
 			<div className="container">
 				<section className="row">
-					<Form 
-						className="col-12 col-md-6 my-form mt-4 mt-md-5"
-						onSubmit={e => e.preventDefault()}
-					>
-						<FormGroup>
-							<h4>Trao đổi luật pháp với mọi người</h4>
-							<p className="text-muted">Cộng đồng tư vấn pháp lí và trao đổi các vấn đề pháp luật lớn nhất Việt Nam</p>
-						</FormGroup>
+					<div className="col-12 col-md-6 my-4 my-md-5">
+						<Form 
+							className="my-form"
+							onSubmit={e => e.preventDefault()}
+						>
+							<FormGroup>
+								<h4>Trao đổi luật pháp với mọi người</h4>
+								<p className="text-muted">Cộng đồng tư vấn pháp lí và trao đổi các vấn đề pháp luật lớn nhất Việt Nam</p>
+							</FormGroup>
 
-						<FormGroup>
-							<input type="text" className="inp-text-rounded" placeholder="Tài khoản của bạn" />
-							<input type="password" className="inp-text-rounded" placeholder="Mật khẩu của bạn" />
-							<CustomInput 
-								className="text-muted"
-								type="switch" 
-								id="exampleCustomSwitch" 
-								name="customSwitch" 
-								label="Nhớ tài khoản" 
-							/>
-						</FormGroup>	
-						<FormGroup>
-							<button 
-								conClick={e => this.handleLogin(e)} 
-								className="cus-button"
-							>Đăng nhập</button>
-						</FormGroup>
-						<FormGroup className="cus-form-group">
-							<Link to="/">Quên mật khẩu?</Link>
-							<Link to="/">Đăng kí ngay?</Link>
-						</FormGroup>
-						<FormText className="cus-form-text">
-							<p>Hoặc</p> 
-						</FormText>
-						<FormGroup>
-							<button className="fb-btn">Đăng nhập với Facebook</button>
-						</FormGroup>
-					</Form>
-					<div className="col-12 col-md-6 mt-4 mt-md-5">
-						
+							<FormGroup>
+								<input type="text" className="inp-text-rounded" placeholder="Tài khoản của bạn" />
+								<input type="password" className="inp-text-rounded" placeholder="Mật khẩu của bạn" />
+								<CustomInput 
+									className="text-muted"
+									type="switch" 
+									id="exampleCustomSwitch" 
+									name="customSwitch" 
+									label="Nhớ tài khoản" 
+								/>
+							</FormGroup>	
+							<FormGroup>
+								<button 
+									conClick={e => this.handleLogin(e)} 
+									className="cus-button"
+								>Đăng nhập</button>
+							</FormGroup>
+							<FormGroup className="cus-form-group">
+								<Link to="/">Quên mật khẩu?</Link>
+								<Link to="/">Đăng kí ngay?</Link>
+							</FormGroup>
+							<FormText className="cus-form-text">
+								<p>Hoặc</p> 
+							</FormText>
+							<FormGroup>
+								<button className="fb-btn">Đăng nhập với Facebook</button>
+							</FormGroup>
+						</Form>
+					</div>
+					
+					<div className="col-12 col-md-6 my-4 my-md-5">
+						<img src="https://picsum.photos/400/300" alt=""className="des"/>
 					</div>
 				</section>
 
